09b4bdde0c1fb962010017fbb10aaff827706b85
Will Harris-Braun <willharrisbraun@gmail.com>
2021-11-29 13:22:23 -0500

    Move instructions to Readme

--- crawler.py
+++ crawler.py
@@ -28,19 +28,3 @@ def crawl():
                 f.write(t_transcription)
         except:
             print(f"Couldn't find corresponding translation for handwritten text {tichaID}!")
-
-# How to use this crawler to download Handwritten Text transcriptions raw:
-#
-# 1. Uplodad this file to the server at:
-#    ticha.haverford.edu:/srv/ticha-django-site
-# 2. On the server, run
-#    `$ source /usr/local/lib/python-virtualenv/ticha-django/bin/activate`
-#    to activate the virtual environment
-# 3. Run `$ python manage.py shell` to start the shell inside the project
-# 4. Run `>>> import crawler` to import this code
-# 5. Run `>>> crawler.crawl()`
-# 6. Exit and check the `crawl-output` folder for your output
-# 7. Move the `crawl-output` folder to your home directory on the server:
-#    `$ cp -r crawl-output ~/`
-# 8. (Clean up after yourself: `$ rm -r crawler.py crawl-output`)
-# 9. From your computer, remote copy the output: `$ scp -r username@ticha.haverford.edu:~/crawl-output .`
